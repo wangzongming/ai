ffb5e66bbcf977cee29fa2c5317e7cb5e20bd8d7
xm <1746809408@qq.com>
2023-02-25 18:38:15 +0800

    update

--- mark-write-code/index.js
+++ mark-write-code/index.js
@@ -2,18 +2,27 @@
 const path = require("path")
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 88
 
 
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration,OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    apiKey: "sk-xxx",
+    // apiKey: "sk-xxx",
+    // 别提交到 gitub
+    apiKey: "sk-",
 });
 const openai = new OpenAIApi(configuration);
 
-app.use(express.static(path.join(__dirname, "./web")))
+// async function test(){ 
+//     console.log("测试")
+//     const response = await openai.listFiles();
+//     console.log('response', response)
+// }
+// test()
 
-app.all("*", function (req, res, next) {
+app.use(express.static(path.join(__dirname,"./web")))
+
+app.all("*",function (req,res,next) {
     console.log(req.url)
     // 除了不校验 token 的接口外，其他接口都需要传入 token 
     res.set({
@@ -33,7 +42,8 @@ var allComm = {};
 var allCode = {};
 var userTimer = {};
 
-app.get('/ai', async (req, res) => {
+
+app.get('/ai',async (req,res) => {
     const q = req.query.q;
     const userId = req.query.userId || "fk";
     clearTimeout(userTimer[userId]);
@@ -45,65 +55,21 @@ app.get('/ai', async (req, res) => {
 
     console.log(`正在请求：${userId} ： ${q}`)
 
+    const oldCode = allCode[userId];
     try {
         const response = await openai.createCompletion({
             model: "code-davinci-002",
-            prompt: `<|endoftext|>
-            /* 
-                I start with id is preview Element, 
-                and incrementally modify it via <script> injection. 
-                All images are available at https://www.pexels.com/.
-                Written for Chrome. 
-            */
-           
-           /* 
-                Command: Add \"Hello World\", 
-                by adding an HTML DOM node 
-            */
-            var helloWorld = document.createElement('div');
-            helloWorld.innerHTML = 'Hello World';
-            document.getElementById("preview").appendChild(helloWorld);
-    
-            /* Command: change "Hellow World" color to red. */
-            helloWorld.style.color = "red"; 
-    
-            /* Command: Clear the page. */
-            while (document.getElementById("preview").firstChild) {
-                document.getElementById("preview").removeChild(document.getElementById("preview").firstChild);
-            }
-    
-            ${allComm[userId]}
-    
-            /* Command: ${q} */\n`,
+            prompt: createPrompt(allComm[userId],q),
             stop: "/* Command:",
-            temperature: 0,
+            temperature: 0.2,
             max_tokens: 1000,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
         });
         const resCode = response.data.choices[0].text;
-        console.log("返回：\n", resCode);
-
-
-        // 记录命令
-        allComm[userId] += ` 
-        
-        /* Command: ${q} */
-        ${resCode}
-    
-        `;
-
-
-        // 代码
-        const oldCode = allCode[userId];
-        allCode[userId] += ` 
-        
-    // ${q} 
-    ${resCode}
-    
-        `;
-
+        logRes(userId, q, resCode)
+        console.log("返回：\n",resCode);
         res.send({
             allCode: allCode[userId],
             newCode: resCode,
@@ -114,17 +80,121 @@ app.get('/ai', async (req, res) => {
         userTimer[userId] = setTimeout(() => {
             allCode[userId] = "";
             allComm[userId] = "";
-        }, 1000 * 60 * 5)
+        },1000 * 60 * 5)
     } catch (err) {
+        console.log('错误：', err)
         res.send({
             allCode: allCode[userId],
             newCode: "// 服务发生了错误 ...",
-            oldCode: allCode[userId]
-        }); 
+            oldCode
+        });
     }
 
 })
 
-app.listen(port, () => {
+app.listen(port,() => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+/**
+ * 记录返回的代码
+*/
+function logRes(userId, q, resCode) {
+    // 记录命令
+    allComm[userId] += `\n/* Command: ${q} */\n${resCode}`;
+
+    // 代码
+    allCode[userId] += `\n// ${q}\n${resCode}\n`;
+}
+
+/**
+ * 创建问题
+ * 单纯 html 代码版本
+*/
+// function createPrompt(userCommand,q) {
+//     return `<|endoftext|>
+// /* 
+//     I start with id is preview Element, 
+//     and incrementally modify it via <script> injection. 
+//     All images are available at https://www.pexels.com/.
+//     Written for Chrome. 
+// */
+
+// /* 
+//     Command: Add \"Hello World\", 
+//     by adding an HTML DOM node 
+// */
+// var helloWorld = document.createElement('div');
+// helloWorld.innerHTML = 'Hello World';
+// document.getElementById("preview").appendChild(helloWorld);
+
+// /* Command: change "Hellow World" color to red. */
+// helloWorld.style.color = "red"; 
+
+// /* Command: Clear the page. */
+// while (document.getElementById("preview").firstChild) {
+//     document.getElementById("preview").removeChild(document.getElementById("preview").firstChild);
+// }
+
+// ${userCommand}
+
+// /* Command: ${q} */\n`
+// }
+
+
+function createPrompt(userCommand,q) {
+    return `<|endoftext|>
+/*   
+    当需要绘制或者创建一个事物时，将使用 javascript 实现，所有的图片将从 https://www.pexels.com/ 获取 并且将写好的代码添加到 #preview 的元素中，这种情况下为Chrome浏览器编写代码。
+    当用户所需要是写一段不是前端的代码将自动用合适的语言实现，并且将代码前一行增加 /* 请复制出去执行，最后增加 */。
+*/
+
+/* 
+    Command: Add \"Hello World\", 
+    by adding an HTML DOM node 
+*/
+var helloWorld = document.createElement('div');
+helloWorld.innerHTML = 'Hello World';
+document.getElementById("preview").appendChild(helloWorld);
+
+/* Command: change "Hellow World" color to red. */
+helloWorld.style.color = "red"; 
+
+/* Command: Clear the page. */
+while (document.getElementById("preview").firstChild) {
+    document.getElementById("preview").removeChild(document.getElementById("preview").firstChild);
+}
+
+/* Command: 写一段 java 的循环 */
+/*
+public class Test {
+    public static void main(String[] args) {
+        int x = 10;
+        while( x < 20 ) {
+            System.out.print("value of x : " + x );
+            x++;
+            System.out.print("\n");
+        }
+    }
+}
+*/
+
+/* Command: 写一段 c 的循环 */
+/*
+#include <stdio.h>
+ 
+int main ()
+{
+   for( ; ; )
+   {
+      printf("该循环会永远执行下去！\n");
+   }
+   return 0;
+}
+*/
+ 
+${userCommand}
+
+/* Command: ${q} */\n`
+}
---
+++ test/web/main.js
@@ -0,0 +1,154 @@
+window.onload = () => {
+    const submitBtn = document.querySelector("#submit");
+    const previewDom = document.querySelector("#preview");
+    const codeDom = document.querySelector("#code");
+    const inputDom = document.querySelector("#input");
+
+    var ing = false;
+    // 用户
+    const userId = getUUID();
+
+    submitBtn.onclick = async () => {
+        const val = inputDom.value;
+        if (ing) return;
+        if (!val || val.length < 5) {
+            submitBtn.innerHTML = "提交(请输入大于 5 个字的问题)";
+            return;
+        }
+
+        submitBtn.innerHTML = "思考中...";
+        // 清空输入框
+        inputDom.value = "";
+
+        var allCode = codeDom.innerText;
+
+        ing = true;
+        const res = await getAns(val,userId)
+        const newCode = res.newCode;
+
+        submitBtn.innerHTML = "编码中...";
+        // 逐个输入
+        const codeArr = `// ${val}\n${newCode}`.split("");
+        const speed = 30;
+        var allT = codeArr.length * speed;
+        codeArr.forEach((t,i) => {
+            setTimeout(() => {
+                enterAni(t);
+
+                allCode += `${t}`;
+                const highlightedCode = hljs.highlight(allCode,{
+                    language: 'javascript'
+                }).value;
+                codeDom.innerHTML = highlightedCode;
+                codeDom.scrollTo(0,codeDom.scrollHeight);
+            },speed * i);
+        });
+
+        // 执行代码
+        setTimeout(() => {
+            // 清空预览区域所有元素
+            while (previewDom.firstChild) {
+                previewDom.removeChild(previewDom.firstChild);
+            }
+
+            eval(res.allCode);
+            
+            // 如果执行失败就不执行
+            // try {
+            //     eval(res.allCode);
+            // } catch (err) {
+            //     // 当时旧代码还是得执行
+            //     // eval(res.allCode);
+            // }
+            ing = false;
+            submitBtn.innerHTML = "提交";
+        },allT);
+    };
+
+    // 按键监听
+    window.addEventListener("keydown",(e) => {
+        const realDownK = e.code.toLocaleLowerCase()
+            .replace(/(key|digit|numpad)/,"")
+        enterAni(realDownK);
+    })
+}
+
+/**
+ * 获取结果
+*/
+async function getAns(val,userId) {
+    return new Promise((resolve) => {
+        //   fetch(`https://wangzongming.top/ai/ai?q=${val}&userId=${userId}`)
+        fetch(`/ai?q=${val}&userId=${userId}`)
+            .then((res) => res.json())
+            .then((res) => resolve(res))
+    })
+}
+
+/**
+ * 创建用户id
+*/
+function getUUID(val,userId) {
+    return `user_${parseInt(Math.random() * 10000000 + 1)}_${parseInt(Math.random() * 10000 + 1)}_${parseInt(Math.random() * 1000 + 1)}`;
+
+}
+
+
+// 音效dom
+const audioDom = document.querySelector("#enterAudio");
+audioDom.playbackRate = 4;
+var audioDomTime;
+
+/**
+ * 按键特效
+ * @t 输入的文字
+*/
+function enterAni(t) {
+    let keyDom;
+    if (/[a-z]/g.test(`${t}`.toLocaleLowerCase())) {
+        keyDom = document.querySelector(`#key-${t}`);
+    } else {
+        // 特殊符号
+        const strMap = [
+            [["1","!"],"49"],
+            [["2","@"],"50"],
+            [["3","#"],"51"],
+            [["4","$"],"52"],
+            [["5","%"],"53"],
+            [["6","^"],"54"],
+            [["7","&"],"55"],
+            [["8","*"],"56"],
+            [["9","("],"57"],
+            [["0",")"],"48"],
+            [["_","-"],"189"],
+            [["+","="],"187"],
+            [["{","["],"219"],
+            [["}","]"],"221"],
+            [["|","\\"],"220"],
+            [[";",":"],"186"],
+            [['"',"'"],"222"],
+            [["<",","],"188"],
+            [[">","."],"190"],
+            [["?","/"],"191"],
+        ];
+        const tag = strMap.find((item) => item[0].includes(t));
+        if (tag) {
+            keyDom = document.querySelector(`#key-${tag[1]}`);
+        }
+    }
+    if (keyDom) {
+        keyDom.setAttribute("class",`${keyDom.className} entering`);
+        setTimeout(() => {
+            keyDom.setAttribute("class",`${keyDom.className.replace("entering","")}`);
+        },50);
+    }
+
+    // audioDom.play();
+
+    audioDom.pause && audioDom.play();
+    clearTimeout(audioDomTime)
+    audioDomTime = setTimeout(() => {
+        audioDom.pause();
+    },100);
+
+}
